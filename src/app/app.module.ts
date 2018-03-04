d29be0f12d090f321f7f0bff32d0fc1bc086b0cd
Oliver Ni <oliver.ni@gmail.com>
2018-03-03 16:03:30 -0800

    Add bootstrap and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RouterModule } from '@angular/router';
 import { routes } from './app-routing';
 
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './pages/index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
   providers: [],
